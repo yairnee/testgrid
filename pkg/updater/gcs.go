a4cb789c652dada37045ee9edb9bc412cd2e7f43
yairnee <yairnee@google.com>
2021-06-22 12:46:51 +0000

    Test grid dynamic email list.
    The idea is that the user will be able to set a dynamic email list for alert.

--- pkg/updater/gcs.go
+++ pkg/updater/gcs.go
@@ -48,6 +48,8 @@ type gcsResult struct {
 
 const maxDuplicates = 20
 
+const EmailListKey = "EmailAddresses"
+
 var overflowCell = Cell{
 	Result:  statuspb.TestStatus_FAIL,
 	Icon:    "...",
@@ -385,6 +387,19 @@ func convertResult(log logrus.FieldLogger, nameCfg nameConfig, id string, header
 		}
 		out.Column.Extra = append(out.Column.Extra, val)
 	}
+
+	emailAddressesInterface, ok := result.finished.Finished.Metadata[EmailListKey]
+	if ok {
+		emailAddresses, ok := emailAddressesInterface.([]string)
+		if ok {
+			out.Column.EmailAddresses = emailAddresses
+		} else {
+			out.Column.EmailAddresses = []string{}
+		}
+	} else {
+		out.Column.EmailAddresses = []string{}
+	}
+	return out
 
 	return out
 }
